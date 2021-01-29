b12e6d374d6c528c56224be6260295fdf4216a82
Pedro Benevides <pedrobenevides3243@gmail.com>
2021-01-29 11:12:53 -0300

    navBar Finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,20 @@ import Navbar from './components/navbar/navbar'
 function App() {
   return (
     <div>
-      <Navbar children={<div></div>}></Navbar>
+      <Navbar children={<div>teste</div>}></Navbar>
+      <div>
+        {
+          // gerador()
+        }
+      </div>
     </div>
   );
 }
-
+const gerador = () => {
+  let res = [];
+  for (let i = 0; i < 1000; i++) {
+    res.push(<p>numero {i}</p>)
+  }
+  return res
+}
 export default App;
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -5,8 +5,6 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
-import Box from '@material-ui/core/Box';
-import Container from '@material-ui/core/Container';
 import Fab from '@material-ui/core/Fab';
 import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 import Zoom from '@material-ui/core/Zoom';
@@ -65,22 +63,10 @@ export default function BackToTop(props: Props) {
             <CssBaseline />
             <AppBar>
                 <Toolbar>
-                    <Typography variant="h6">Scroll to see button</Typography>
+                    <Typography variant="h6">Pedro Benevides</Typography>
                 </Toolbar>
             </AppBar>
             <Toolbar id="back-to-top-anchor" />
-            <Container>
-                <Box my={2}>
-                    {[...new Array(12)]
-                        .map(
-                            () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
-                        )
-                        .join('\n')}
-                </Box>
-            </Container>
             <ScrollTop {...props}>
                 <Fab color="secondary" size="small" aria-label="scroll back to top">
                     <KeyboardArrowUpIcon />
